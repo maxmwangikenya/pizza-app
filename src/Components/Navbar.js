ba04fc9321cada5d8278e35c051205ed54f0af46
maxmwangikenya <maxmwangikenya@gmail.com>
2024-10-25 18:39:19 +0300

    navBar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import Logo from '../Assets/pizzaLogo.png'
-import {Link} from "react-router-dom";
-import '../Styles/Navbar.css'
+import React, { useState } from 'react';
+import Logo from '../Assets/pizzaLogo.png';
+import { Link } from "react-router-dom";
+import '../Styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
-  return (
-    <div className='navbar'>
-        <div className='leftside'>
-            <img src={Logo} />
+    const [openLinks, setOpenLinks] = useState(false); 
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks); 
+    };
+
+    return (
+        <div className='navbar'>
+            <div className='leftside' id={openLinks ? "open" : "close"}> {/* Corrected here */}
+                <img src={Logo} alt="Pizza Logo" />
+                <div className='hiddenlinks'></div>
+            </div>
+            <div className='rightside'>
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
+                </button>
+            </div>
         </div>
-        <div className='rightside'>
-            <Link to="/">Home</Link>
-            <Link to="/menu">menu</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">contact</Link>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="home">
+      <div className="headerContainer">
+        <h1>Maxi's Pizzeria</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div> 
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
